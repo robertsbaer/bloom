63099bf890eabcd088e65ab4588ff0a358b117b0
robertsbaer <robertsbaer@yahoo.com>
2026-06-22 22:19:26 -0400

    Restore GitHub Pages deployment config

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/wholesale" element={<WholesalePage />} />
