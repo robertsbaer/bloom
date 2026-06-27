aafb214285526fbd37f434776261166882698126
robertsbaer <robertsbaer@yahoo.com>
2026-06-27 16:13:35 -0400

    supabase com and deploy

--- supabase/functions/request-password-reset/index.ts
+++ supabase/functions/request-password-reset/index.ts
@@ -1,4 +1,5 @@
 // @ts-nocheck
+// v1.0.1
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import { corsHeaders } from "../_shared/cors.ts";
