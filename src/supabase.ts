c2e71f69e3872bfdffbdf66571ee2f709adb6d39
robertsbaer <robertsbaer@yahoo.com>
2026-06-26 23:52:19 -0400

    fix: remove faulty database trigger

--- src/supabase.ts
+++ src/supabase.ts
@@ -3,6 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
 const supabaseAnon = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
+if (!supabaseUrl || !supabaseAnon) {
+  throw new Error("Supabase URL and anonymous key are required.");
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnon, {
   auth: {
     persistSession: true,
